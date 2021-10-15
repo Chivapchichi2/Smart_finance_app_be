a2519393eb6589459069406dbe9db901aa0cec51
unknown <ivanfiliptschik00@gmail.com>
2021-10-15 20:58:56 +0300

    fixed_reports

--- helpers/updateReport.js
+++ helpers/updateReport.js
@@ -3,12 +3,14 @@ const updateLedgerReport = (year, month, value, lastLedger) =>
   lastLedger[year] !== undefined
     ? {
         ...lastLedger,
-        [year]: { [month]: String(+lastLedger[year][month] + +value) },
+        [year]: lastLedger[year][month]
+          ? { ...lastLedger[year], [month]: String(+lastLedger[year][month] + +value) }
+          : { ...lastLedger[year], [month]: String(value) },
       }
     : {
         ...lastLedger,
-      [year]: { [month]: String(value) }
-    };
+        [year]: { [month]: String(value) },
+      };
 
 //create the new obj for reports by remove transaction
 const updateReportsAfterRemove = (year, month, value, lastLedger) => {
